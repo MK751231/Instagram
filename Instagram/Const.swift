346eaa1890aa305a7fc41ca86be5b3c33fb0330a
masato.koshikawa <koshikawa-m@nespe.com>
2021-09-03 16:36:21 +0900

    最初のコミット

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 越川将人 on 2021/09/02.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
